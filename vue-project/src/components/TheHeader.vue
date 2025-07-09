f0389a0c3a0d6fc32725da2792fc45468047ed17
domidom404 <dominicoliveira.404@gmail.com>
2025-07-09 17:45:56 -0300

    feat: aula 07 - Atributos Class e Style dinâmicos

--- vue-project/src/components/TheHeader.vue
+++ vue-project/src/components/TheHeader.vue
@@ -1,12 +1,30 @@
 <template>
+  <div>
+  <!--como fazer o bind com class e style-->
+  <div>
+
+    <h1 :class="{'title': true, 'title-home': isHome}">
+      Curso Vue 3
+    </h1>
+
+    <p :class="pClass">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum rem tempora quasi pariatur reprehenderit voluptatum, neque, laudantium harum deserunt reiciendis similique aliquam. Deleniti, quam quibusdam velit molestiae iure voluptas itaque.
+    </p>
+
+<!--pode usar qualquer atributo do css usando o style-->
+    <p :style="styleClass">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus cumque neque a voluptatum earum, voluptates facilis amet voluptas tempore! Nisi libero consequuntur ullam ab! Ex velit corporis maxime necessitatibus nostrum.
+    </p>
+
+
+  </div>
+
 <div>
-<!--v-bind instrui o atributo para que ele fique dinamico, sendo uma variavel com valor-->
   <div 
   v-for="(obj, index) in todos"  
   v-bind:key= "obj.id"
   class="todo-item"
   >
-  <!--Usar apenas ':' é suficiente pro codigo entender que vc ta usando v-bind e deixa o codigo mais limpo-->
   <img 
   v-if="obj.imgSrc"
   :src="obj.imgSrc">
@@ -14,6 +32,7 @@
     
   </div>
 </div>
+</div><!--coloquei uma div extra pra emglobar as outras e ficar menos 'solto'-->
 </template>
 
 <script>
@@ -21,15 +40,23 @@
       name: 'App',
       data() {
         return {
-          todos: [ //foram pegos no site https://jsonplaceholder.typicode.com/todos?_limit=5
+          isHome: true,
+          classVar: 'title',
+          //se nao quiser escrever background-color pode escrever tirando o - e com a 2 palavra em maiusculo
+          styleClass: {'color': 'aqua', 'backgroundColor': 'black', 'font-size': '20px'},
+//prof havia colocado apenas 'pClass: {...}' e tava dando erro pq a variavel ainda nao estava acessivel a esse ponto
+//sendo uma função, ela é acessada no momento que o Vue renderiza o template
+          pClass(){
+            return ['text', {'text-home': this.isHome}];
+          },
+          todos: [ 
   {
     "userId": 1,
     "id": 1,
     "title": "delectus aut autem",
     "completed": false,
     "imgSrc":'https://placehold.co/150',
-  },//uma imagem p usuario; precisa usar aspas pra declarar variavel dentro de um objeto
-  //o site q ele usou 'placeholder.com' nao esta mais disponivel ent usei esse no lugar
+  },
   {
     "userId": 1,
     "id": 2,
@@ -59,15 +86,34 @@
         }
       }
      }
+     
 </script>
 
 <style>
+.title{
+  font-size: 20px;
+  color: blue;
+}
+
+.title-home{
+  font-size: 40px;
+  color: green;
+}
+
+.text{
+  color: yellow;
+}
+
+.text-home{
+  color: seagreen;
+}
 .todo-item {
   background: #000;
   margin: 0 0 5px 0;
   padding: 3px 6px;
   color: #fff;
 }
+
 #app{
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
