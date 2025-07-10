f2911f5ab6802d1476bc92a0e09a0acb4167ec58
domidom404 <dominicoliveira.404@gmail.com>
2025-07-10 19:33:22 -0300

    feat: Aula 09 - Eventos

--- vue-project/src/components/TheHeader.vue
+++ vue-project/src/components/TheHeader.vue
@@ -1,88 +1,34 @@
 <template>
-  <div> 
-    <div>
-      -one way data binding- <br>
-      -two way data binding- <br> <!--ou ligação bidirecional de dados-->
-      -v-model -> formularios- <!--comando utilizado pro two way bd-->
+  <div><!--vc clica no botao 'enviar' e ativa o evento-->
+    <!--para isso usamos a diretiva 'v-on', capturando quando tiver um clique no evento-->
+    <!-- 'v-on' pode ser substituido por '@' para encurtar o cod-->
+    <button @click.once="onClick"> <!-- .once so registra o clique 1x-->
+      Enviar
+    </button>
+    <br>
+<!--registra quando o user passou ou tirou a seta do evento-->
+    <div 
+    @mouseover="onMouseOver"
+    @mouseout="onMouseOut"
+    >
+      Mouse over
     </div>
-<!-- o two way permite q o valor de campo de um formulario e a variavel do JS fiquem sempre sincronizados-->
+    
     <br><br>
-
-    <div>
-      <label>Nome</label> <br>
-          <input 
-           v-model="name"
-           type="text"
-           >
-    {{ name }}
-<!-- se vc muda a variavel no input a variavel muda-->
-    </div>
-
-    <br><br>
-
-    <div>
-      <!--estou misturando ingles e pt pq o prof tbm esta fazendo isso e disse que pe so pra exemplo, estou ciente de que nao devemos fzr isso em codigos serios-->
-      <!--seleciona uma opcçao na lista do formulario-->
-      <label>Sports</label> <br>
-      <select v-model="sports">
-      <option value="">Escolha</option>
-
-      <option value="futebol">Futebol</option>
-      <option value="skate">Skate</option>
-      <option value="tenis">Tenis</option>
-      </select> <br>
-      {{ sports }}
-    </div>
-
-    <br><br>
-
-    <div>
-      <label>NewsLetter</label> <br>
-      <input
-        v-model="newsletter"
-        type="radio"
-        value="Sim"
-        >Sim
-
-        <input 
-        v-model="newsletter"
-        type="radio"
-        value="Não"
-        >Não <br>
-        {{ newsletter }}
-    </div>
-
-    <br><br>
-
-    <div>
-      <!-- seleciona se aceita ou não o contrato-->
-      <label>Contrato</label> <br>
-      <input
-        v-model="contract"
-        type="checkbox"
-        >Aceita nossos termos?... <br>
-
-        {{ contract }}
-    </div>
-
-    <br><br>
-
-      <div>
-      <!--se vc muda a variavel no codigo, o input tb muda automaticamente -->
-      <label>Cores que você mais gosta</label> <br>
-      <input
-        v-model="colors"
-        type="checkbox"
-        value="Azul"
-        >Azul
-
-        <input 
-        v-model="colors"
-        type="checkbox"
-        value="Amarelo"
-        >Amarelo <br>
-        {{ colors }}
-    </div>
+<!--.prevent previne o comportamento padrão do submit, que é recarregar a página, enviando o user pro site google-->
+    <form
+     action="https://google.com"
+     @submit.prevent="onSubmit" 
+     >
+     <input
+      type="text"
+      @keyup="onKeyUp">
+      <button type="submit">
+        Enviar
+      </button>
+    </form>
+<!-- modificadores de eventos: .stop, .prevent, .self, .capture, .once, .passive-->
+ <!--'keyup' registra td digitado pelo user, '.enter' ajuda a so registrar dps de apertar ENTER-->
   </div>
 </template>
 
@@ -91,12 +37,27 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'Jon Snow',//primeira direção, a segunda é oq o usuario colocar no campo
-      sports:'futebol',
-      newsletter: '', //se colocasse 'sim' ou 'nao' aqui, o valor padrão seria esse, mas como deixei vazio, o valor padrão é vazio
-      contract: true,
-      colors: [],//captura opções no array, se nao fizer isso a checkbox vem marcada ambas as opções de uma vez
     }
+  },
+
+  methods:{ 
+  //objeto que colcoamos nossas funções pra fazer qqlr rotina
+  onClick($evt){
+    console.log('click', $evt);
+   }, 
+   onMouseOver($evt){
+    console.log('mouse over', $evt);
+   },
+   onMouseOut($evt){
+    console.log('mouse out', $evt);
+   },
+   onSubmit($evt){
+    console.log('submit', $evt);
+   },
+   onKeyUp($evt){
+    console.log('onKeyUp', $evt);
+   }
+  //'$evt' é o evento que foi disparado, e podemos usar ele pra pegar detalhes do evento, como o tipo de evento, o alvo do evento, etc
   }
 }
 
