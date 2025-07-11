beedea9d8391a0cad66ce287e28e7fdd8613e915
domidom404 <dominicoliveira.404@gmail.com>
2025-07-11 17:31:58 -0300

    feat: aula 11 – Observadores (watch)

--- vue-project/src/components/TheHeader.vue
+++ vue-project/src/components/TheHeader.vue
@@ -1,42 +1,32 @@
 <template>
   <div>
-    {{ fullName }} <br> <br>
-    <!--computed properties(propriedades computadas)-->
-
-    <h2>Todos em aberto</h2>
-    <div
-     v-for="todo in uncompletedTodos"
-     :key="todo.id" 
-     > <!--:key -> chave unica usado pra renderizar elementos corretamente conforme atualizações -->
-    {{ todo.title }} 
-    </div>
+    <!-- Formulários reativo com v-model para inserção de dados -->
+    <input 
+    v-model="name"
+    type="text">
+    <br><br>
+    {{  name }}
 
     <br><br>
 
-    <h2>Todos completas</h2>
-     <div
-     v-for="todo in completedTodos"
-     :key="todo.id"
-     >
-    {{ todo.title }} 
-    </div>
+    <input 
+    v-model="user.first_name"
+    type="text">
+    <br>
+    
+    <input 
+    v-model="user.last_name"
+    type="text">
+    <br>
+     {{  user.first_name }} {{  user.last_name }}
 
     <br><br><br>
-
-    <h2>Todos</h2>
-    <div
-     v-for="todo in todos"
-     :key="todo.id"
-     >
-<!--'v-model' faz 2way data binding entre checkbox e a propriedade 'completed' de kd todo-->
-     <input
-      v-model="todo.completed"
-      type="checkbox"
-      >
-<!--a reatividade ta funcionando quando marco ou desmarco um checkbox o item sai ou entra p aberto/fechado -->
-    {{ todo.title }} 
-    </div>
-
+    <select v-model="pageCount">
+      <option value="5">5</option>
+      <option value="10">10</option>
+      <option value="15">15</option>
+    </select> <br>
+    {{ pageCount }}
   </div>
 </template>
 
@@ -45,73 +35,50 @@ export default {
   name: 'App',
   data() {
     return {
-      user:{
-        first_name: 'Jon',
-        last_name: 'Snow',
-      },
-//se o array 'todo' for alterado 'uncompletedTodos' e 'completedTodos' serão recomputados
-      todos: [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
+      name: '',
+      pageCount: 5,//valor inicial do select
+      user: {
+        first_name: '',
+        last_name: '',
+      } 
+
   }
-]
-    }
-  },
-//proxy é onde o vue.js faz toda a reatividade dele
-// 'this' -> como acessar dados do user dentro das outras funções do componente
+},
+// em vuejs, watch é útil quando você precisa realizar operações assíncronas ou complexas em resposta a alterações de dados, 
+// ou quando você precisa acessar os valores antigos e novos da propriedade observada
+  watch:{
+//iremos observar uma coisa, e assim que se alterar executamos uma rotina de programação
+    name(vl){
+      if(vl.length >= 3) {//so registra quando tiver 3 caracteres ou mais
+        this.saveUserName() 
+      }
+    },
+    pageCount(){
+      this.changePage();
+    },
+
+    user: { //como so os campos estao sendo altrados, temos que colocar user como objeto, nao como função
+      handler(){
+        console.log('User alterado');
+      },
+      deep: true //para observar objetos dentro do objeto
+    },
+},
+
   computed:{
-//Dependência quando o valor final depende de outros valores
-    fullName(){
-      return `${this.user.first_name} ${this.user.last_name}`//'template srting pra concatenar'
-//precisamos retornar pq a função sera tratada como uma função normal
-    },
-    uncompletedTodos(){
-      return this.todos.filter( todo => !todo.completed);
-      //filtra os todos incompletos (só onde aparece "completed":false aparecerá )
-    },
-    completedTodos() {
-      return this.todos.filter( todo => todo.completed);
-      //filtra os todos completos (só onde aparece "completed":true aparecerá )
-   
-    },
-
-
+    
   },
 
-  methods:{ 
-    click() {
-      console.log(this.user);
+  methods: {
+    saveUserName(){
+      console.log('Ajax'); //Ajax para salvar o nome
+      console.log(this.name);
+    },
+    changePage(){
+      console.log('Ajax changePage');
     }
  
-  },
-
+  }
 }
 
 </script>
