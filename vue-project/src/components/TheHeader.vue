690e1470d830b585ad21c4146a790cb48b1f6393
domidom404 <dominicoliveira.404@gmail.com>
2025-07-10 21:28:39 -0300

    feat: Aula 10 – Propriedade computada (computed)

--- vue-project/src/components/TheHeader.vue
+++ vue-project/src/components/TheHeader.vue
@@ -1,34 +1,42 @@
 <template>
-  <div><!--vc clica no botao 'enviar' e ativa o evento-->
-    <!--para isso usamos a diretiva 'v-on', capturando quando tiver um clique no evento-->
-    <!-- 'v-on' pode ser substituido por '@' para encurtar o cod-->
-    <button @click.once="onClick"> <!-- .once so registra o clique 1x-->
-      Enviar
-    </button>
-    <br>
-<!--registra quando o user passou ou tirou a seta do evento-->
-    <div 
-    @mouseover="onMouseOver"
-    @mouseout="onMouseOut"
-    >
-      Mouse over
+  <div>
+    {{ fullName }} <br> <br>
+    <!--computed properties(propriedades computadas)-->
+
+    <h2>Todos em aberto</h2>
+    <div
+     v-for="todo in uncompletedTodos"
+     :key="todo.id" 
+     > <!--:key -> chave unica usado pra renderizar elementos corretamente conforme atualizações -->
+    {{ todo.title }} 
     </div>
-    
+
     <br><br>
-<!--.prevent previne o comportamento padrão do submit, que é recarregar a página, enviando o user pro site google-->
-    <form
-     action="https://google.com"
-     @submit.prevent="onSubmit" 
+
+    <h2>Todos completas</h2>
+     <div
+     v-for="todo in completedTodos"
+     :key="todo.id"
      >
+    {{ todo.title }} 
+    </div>
+
+    <br><br><br>
+
+    <h2>Todos</h2>
+    <div
+     v-for="todo in todos"
+     :key="todo.id"
+     >
+<!--'v-model' faz 2way data binding entre checkbox e a propriedade 'completed' de kd todo-->
      <input
-      type="text"
-      @keyup="onKeyUp">
-      <button type="submit">
-        Enviar
-      </button>
-    </form>
-<!-- modificadores de eventos: .stop, .prevent, .self, .capture, .once, .passive-->
- <!--'keyup' registra td digitado pelo user, '.enter' ajuda a so registrar dps de apertar ENTER-->
+      v-model="todo.completed"
+      type="checkbox"
+      >
+<!--a reatividade ta funcionando quando marco ou desmarco um checkbox o item sai ou entra p aberto/fechado -->
+    {{ todo.title }} 
+    </div>
+
   </div>
 </template>
 
@@ -37,28 +45,73 @@ export default {
   name: 'App',
   data() {
     return {
+      user:{
+        first_name: 'Jon',
+        last_name: 'Snow',
+      },
+//se o array 'todo' for alterado 'uncompletedTodos' e 'completedTodos' serão recomputados
+      todos: [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  }
+]
     }
+  },
+//proxy é onde o vue.js faz toda a reatividade dele
+// 'this' -> como acessar dados do user dentro das outras funções do componente
+  computed:{
+//Dependência quando o valor final depende de outros valores
+    fullName(){
+      return `${this.user.first_name} ${this.user.last_name}`//'template srting pra concatenar'
+//precisamos retornar pq a função sera tratada como uma função normal
+    },
+    uncompletedTodos(){
+      return this.todos.filter( todo => !todo.completed);
+      //filtra os todos incompletos (só onde aparece "completed":false aparecerá )
+    },
+    completedTodos() {
+      return this.todos.filter( todo => todo.completed);
+      //filtra os todos completos (só onde aparece "completed":true aparecerá )
+   
+    },
+
+
   },
 
   methods:{ 
-  //objeto que colcoamos nossas funções pra fazer qqlr rotina
-  onClick($evt){
-    console.log('click', $evt);
-   }, 
-   onMouseOver($evt){
-    console.log('mouse over', $evt);
-   },
-   onMouseOut($evt){
-    console.log('mouse out', $evt);
-   },
-   onSubmit($evt){
-    console.log('submit', $evt);
-   },
-   onKeyUp($evt){
-    console.log('onKeyUp', $evt);
-   }
-  //'$evt' é o evento que foi disparado, e podemos usar ele pra pegar detalhes do evento, como o tipo de evento, o alvo do evento, etc
-  }
+    click() {
+      console.log(this.user);
+    }
+ 
+  },
+
 }
 
 </script>
@@ -69,7 +122,7 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #4e7092;
   margin: 60px;
 }
 </style>
