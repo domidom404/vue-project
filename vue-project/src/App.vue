404beded56bbe0241b3e9400c1db3453dc30b8a4
domidom404 <dominicoliveira.404@gmail.com>
2025-07-14 18:13:26 -0300

    feat: aula 12 – Ciclo de vida (life cycle)

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,121 +1,101 @@
+<template>
+  <div>
+    <TheHeader v-if="showHeader"/>
+    <h1>Hello World</h1>
+    <br>
+    <input 
+    v-model="name"
+    type="text">
+    {{ name }} <br>
+    <button @click="showHeader = !showHeader">
+      Ativar e desativar header
+    </button>
+  </div>
+</template>
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'//importa o componente TheHeader pro js
-export default{
-  name: 'App', //nome do componente atual, 'App.vue'
-  components: {
-    //registra os componentes que serão mostrados nesse template, no caso sendo HelloWorld e agr nosso cabeçalho TheHeader
-    HelloWorld,
-    TheHeader
+import TheHeader from './components/TheHeader.vue';
+export default {
+  name: 'App',
+  components: {TheHeader},
+  data() {
+    return {
+      name: 'Joao Silva',
+      showHeader: true,
+    }
+  },
+// Criação: 
+  //preparar o componente
+  //Ajax, inicializar algumas variaveis
+  //nao tem acesso ao DOM(template)
+// Montagem:
+  // Inicializar uma lib externa
+  //Geralmente precisa de acesso ao template (DOM)
+  //Tem acesso ao template (DOM)
+// Atualização:
+  //Debug
+  // Update
+// Desmontagem:
+ // Remover tudo que for necessário para liberar memória (lib -> destroy)
+
+ //HOOKS
+ beforeUpdate(){
+  console.log('beforeUpdate', this.name);
+ },
+ updated(){
+  console.log('updated', this.name);
+ },
+ //antes do componente ser formado
+ beforeCreate(){
+  console.log('beforeCreate');
+  console.log('Estado:', this.name);
+  console.log('DOM:', this.$el) //$el puxa o elemento raiz do componente
+ },
+//depois do componente ser formado
+ created(){
+  console.log('created');
+  console.log('Estado:', this.name);
+  console.log('DOM:', this.$el)
+ },
+ 
+ //antes do componente ser montado
+  beforeMount(){
+    console.log('beforeMount');
+    console.log('Estado:', this.name);
+    console.log('DOM:', this.$el)  },
+  //depois que o componente foi montado
+  mounted(){
+    console.log('mounted');
+    console.log('Estado:', this.name);  
+    console.log('DOM:', this.$el)
+    },
+  //antes de montar o componente
+ beforeMount(){
+  console.log('beforeMount');
+ },
+ unmounted(){
+  console.log('unmounted');
+ },
+
+ watch:{
+},
+
+  computed:{
   },
 
-  data(){
-    return {
-      //tudo delcarado aqui sera um objeto reativo, ou seja, se mudar aqui, muda na tela
-      showHeader: true,
-      firstname: 'Jon',
-      lastname: 'Snow',
-      showName: false,
-      acecessLevel: 'admin'
-    }
-  }
+  methods: {
+}
 }
 </script>
 
-<template>
-  <!-- v- são diretivas: dão "direções"/instruções pro cod html-->
-  <!-- diferença entre v-if(+ pesado) e v-show(+eficiente) é que em v-show o objeto permanece no DOM, o v-if remove o componente. Semelhança é que ambos tem função de 'mostrar' e 'esconder' -->
-  <TheHeader v-if="showHeader"/>
-  
-  <header>
-
-  <div v-show="showName">
-    Nome: {{ firstname }} <br> <!-- chave dupla é interpolação, usado p mostrar valores de variáveis-->
-    Sobrenome: {{ lastname }}
-  </div>
-
-  <div v-if="acecessLevel === 'admin'">Usuário Admin</div>
-  <div v-else-if="acecessLevel === 'marketing'">Usuário Marketing</div>
-  <div v-else="">Usuário normal</div>
-  <!--recomendado o uso de if pois usando o v-show daria pra ver o id de outros usuarios ao inspecionar o codigo-->
-  
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">teste</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
 <style>
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#app{
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #4e7092;
+  margin: 60px;
 }
 </style>
--- vue-project/src/components/TheHeader.vue
+++ vue-project/src/components/TheHeader.vue
@@ -1,95 +1,38 @@
 <template>
-  <div>
-    <!-- Formulários reativo com v-model para inserção de dados -->
-    <input 
-    v-model="name"
-    type="text">
-    <br><br>
-    {{  name }}
-
-    <br><br>
-
-    <input 
-    v-model="user.first_name"
-    type="text">
-    <br>
-    
-    <input 
-    v-model="user.last_name"
-    type="text">
-    <br>
-     {{  user.first_name }} {{  user.last_name }}
-
-    <br><br><br>
-    <select v-model="pageCount">
-      <option value="5">5</option>
-      <option value="10">10</option>
-      <option value="15">15</option>
-    </select> <br>
-    {{ pageCount }}
-  </div>
+  <header class="header">
+    Header
+  </header>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      name: '',
-      pageCount: 5,//valor inicial do select
-      user: {
-        first_name: '',
-        last_name: '',
-      } 
-
-  }
-},
-// em vuejs, watch é útil quando você precisa realizar operações assíncronas ou complexas em resposta a alterações de dados, 
-// ou quando você precisa acessar os valores antigos e novos da propriedade observada
-  watch:{
-//iremos observar uma coisa, e assim que se alterar executamos uma rotina de programação
-    name(vl){
-      if(vl.length >= 3) {//so registra quando tiver 3 caracteres ou mais
-        this.saveUserName() 
-      }
-    },
-    pageCount(){
-      this.changePage();
-    },
-
-    user: { //como so os campos estao sendo altrados, temos que colocar user como objeto, nao como função
-      handler(){
-        console.log('User alterado');
-      },
-      deep: true //para observar objetos dentro do objeto
-    },
-},
-
-  computed:{
-    
+export default{
+  mounted(){
+    window.addEventListener('resize', this.resize)
+  },
+  beforeUnmount(){
+    // Destruir as nossas libs
+    // Eventos
+    // Listeners
+  console.log('beforeUnmount');
+  window.removeEventListener('resize', this.resize)
+  },
+  unmonted(){
+    console.log('unmonted');
   },
 
-  methods: {
-    saveUserName(){
-      console.log('Ajax'); //Ajax para salvar o nome
-      console.log(this.name);
-    },
-    changePage(){
-      console.log('Ajax changePage');
-    }
- 
+  methods:{
+    resize($evt){
+      console.log($evt);
+    } 
   }
+  
 }
-
 </script>
 
 <style>
+.header{
+  background: #000;
+  color: #fff;
 
-#app{
-  font-family: Arial, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #4e7092;
-  margin: 60px;
 }
 </style>
