b3ea142669797fd03d235f26a8e158e4939c4272
domidom404 <dominicoliveira.404@gmail.com>
2025-08-04 20:04:12 -0300

    feat: aula 28 – Composition API: Props e Emit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,53 +1,31 @@
 <template>
-  <AppHook v-if="showAppHook"/>
-  <button @click= "showAppHook = !showAppHook" >
-    Toggle <!--botao faz o Hook sumir e aparecer, montando, desmontando e assim por diante-->
-  </button>
-
-  <h5>USER</h5>
-  {{ user.first_name }}
-  {{ user.last_name }}
-  <br><br>
-  <h6>FULLNAME</h6>
-  {{ fullName }}
-  <br><br>
-  <button @click="user.first_name = 'Sol'">Atualizar</button>
+  Hello vue <br>
+  <AppButton 
+  data-vue="Jay"
+  variant="danger"
+  @update="getUpdate"
+  > <!--botao alterações-->
+    Save
+    <template #icon >Icon</template>
+  </AppButton>
 </template>
 
 
 <script>
-import { computed, ref, watch } from 'vue';
-import AppHook from './components/AppHook.vue';
+import AppButton from './components/AppButton.vue';
 
-export default {
+export default{
   name: 'App',
-  components: {
-    AppHook
-  },
-
-  setup() { 
-    const user = ref({ 
-      first_name: 'Jay',
-      last_name: 'Snow'
-    })
-
-    const showAppHook = ref(true)
-
-    const fullName = computed(() => {
-      return `${user.value.first_name} ${user.value.last_name}`
-    })
-
-    watch( () => user.value.first_name, () => { 
-      console.log('viu que o user mudou')
-    }, )
-
-    return{
-      user,
-      fullName,
-      showAppHook
+  components: { AppButton }, //nunca esquecer de declarar components
+  setup() {
+    const getUpdate = (data) => {
+      console.log('getUpdate', data); //imprime o valor do update/dados
     }
 
-  }
+    return{
+      getUpdate,
+    }
+  },
 }
 </script>
 
---
+++ vue-project/src/components/AppButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button @click="sendData"> <!--botao chama a função sendData ao ser clicado-->
+        {{ variant }} <!--repassa o valor da prop variante-->
+        <slot></slot>
+            <slot name="icon" />
+    </button>
+</template>
+
+<script>
+export default{
+    props: {
+        variant:{
+            type: String,
+            default: 'success',
+        },
+    },
+
+    setup(props, {emit}) { // props é de propriedades
+     //slots -> acesso direto ao obj
+     const sendData = () => {
+        emit('update', props.variant) //pegar o valor da propriedade dinamicamente
+    }
+
+    return{
+        sendData
+    }
+}
+
+
+}
+</script>
